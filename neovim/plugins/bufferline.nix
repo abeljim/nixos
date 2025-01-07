cec206ebc5713c674a4b0f2b51946515be4260e5
Abel Jimenez <abelj1@uci.edu>
2025-01-06 19:17:38 -0800

    added transperent bufferline

--- neovim/plugins/bufferline.nix
+++ neovim/plugins/bufferline.nix
@@ -91,8 +91,17 @@
             highlight = "Directory";
             text_align = "left";
           }
-        ];
+        ]; 
       };
+      # For Transparent
+      luaConfig.post = ''
+        vim.g.transparent_groups = vim.list_extend(
+               vim.g.transparent_groups or {},
+               vim.tbl_map(function(v)
+                       return v.hl_group
+               end, vim.tbl_values(require("bufferline.config").highlights))
+        )
+      '';
     };
   };
 

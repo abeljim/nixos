ec5240cd4b07b88cf3e2fb845437012050e4992f
Abel Jimenez <abelj1@uci.edu>
2025-01-07 22:29:46 -0800

    added typos config to repo

--- neovim/plugins/tscomments.nix
+++ neovim/plugins/tscomments.nix
@@ -1,5 +1,7 @@
 {
   programs.nixvim.plugins = {
+    # Handles more complex file where there is more formats in a single file than one
+    # Useful for Vue
     ts-comments = {
       enable = true;
     };

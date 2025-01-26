078d80ecd111b6d2d0d6c0831efdeb3a4939f7c8
abeljim <abel.jimenez@rekovar.com>
2025-01-26 10:28:40 -0800

    server should be ready for testing

--- machines/server/home.nix
+++ machines/server/home.nix
@@ -1,11 +1,30 @@
 {
   # config,
   pkgs,
-  # inputs,
+  inputs,
   ...
 }: {
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "abeljim";
+  home.homeDirectory = "/home/abeljim";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  # -- home.stateVersion = "24.11"; # Please read the comment before changing.
+  nixpkgs.config.allowUnfree = true;
+
   imports = [
-    ../commonhome.nix
+    inputs.nixvim.homeManagerModules.nixvim
+    ../modules/neovim
+    ../modules/zsh
+    ../modules/fish
+    ../modules/clitools
   ];
 
   # This value determines the Home Manager release that your configuration is
@@ -82,5 +101,11 @@
   # or
   #
   #  /etc/profiles/per-user/abeljim/etc/profile.d/hm-session-vars.sh
-  #
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true; #
 }

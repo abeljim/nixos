4209db2496a9b085dcba8e45aecc5c31f646dfcf
Abel Jimenez <abelj1@uci.edu>
2025-02-15 21:19:03 -0800

    added buffer whichkey icon

--- modules/neovim/plugins/whichkey.nix
+++ modules/neovim/plugins/whichkey.nix
@@ -2,6 +2,15 @@
   programs.nixvim.plugins = {
     which-key = {
       enable = true;
+      settings = {
+        spec = [
+          {
+            __unkeyed-1 = "<leader>b";
+            group = "Buffers";
+            icon = "󰓩 ";
+          }
+        ];
+      };
     };
   };
 }

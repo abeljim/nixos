d896d56eeaef25f11e5a198ca217c3468cacd66d
Abel Jimenez <abelj1@uci.edu>
2025-02-16 17:24:39 -0800

    added livegrep

--- modules/neovim/plugins/telescope.nix
+++ modules/neovim/plugins/telescope.nix
@@ -4,4 +4,16 @@
       enable = true;
     };
   };
+
+  programs.nixvim.keymaps = [
+    {
+      mode = "n";
+      key = "<leader>fg";
+      action = "<cmd>Telescope live_grep<cr>";
+      options = {
+        silent = true;
+        desc = "Live grep (root dir)";
+      };
+    }
+  ];
 }

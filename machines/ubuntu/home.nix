947c15da4d5d55a093c8e6e38b4577dd3254fad3
abeljim <abelj1@uci.edu>
2025-07-08 15:53:33 -0700

    added vial to ubuntu

--- machines/ubuntu/home.nix
+++ machines/ubuntu/home.nix
@@ -45,6 +45,7 @@
     pkgs.nerd-fonts.zed-mono
 
     (config.lib.nixGL.wrap pkgs.dsview)
+    (config.lib.nixGL.wrap pkgs.vial)
     #  It is necessary to add the following line to the /etc/udev/rules.d/60-dreamsourcelab.rules file:
     # SUBSYSTEM=="usb", ATTRS{idVendor}=="2a0e", MODE="0666"
   ];
